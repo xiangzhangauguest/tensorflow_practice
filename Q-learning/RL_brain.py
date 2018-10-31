1eb286f7b19197dab32d5dcfa5eed6a2d1af3b2e
zhangxiangjor <zhangxiang0831@gmail.com>
2018-10-31 14:19:10 +0800

    print q table

--- Q-learning/RL_brain.py
+++ Q-learning/RL_brain.py
@@ -37,6 +37,9 @@ class QLearningTable:
         else:
             q_target = r  # next state is terminal
         self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # update
+        print(self.q_table.astype("float16"))
+        print
+        print
 
     def check_state_exist(self, state):
         if state not in self.q_table.index:
